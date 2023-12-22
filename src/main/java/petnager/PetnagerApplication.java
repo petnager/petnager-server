913c029df91491b1b3008552204443ea6ee3e936
anneu <b.imnuj@gmail.com>
2023-12-22 16:19:23 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/petnager/PetnagerApplication.java
@@ -0,0 +1,13 @@
+package petnager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetnagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetnagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/petnager/PetnagerApplicationTests.java
@@ -0,0 +1,13 @@
+package petnager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetnagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
